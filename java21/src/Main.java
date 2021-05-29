55f1d72048d78ca88e29c609cd6ba3abe3ae9502
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2021-05-29 13:06:54 +0300

    Add files via upload

---
+++ java21/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        float amount = 1000;
+        float persent = 20;
+        float bonus = amount / persent;
+
+        System.out.println(bonus);
+
+
+    }
+}
